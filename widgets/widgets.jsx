1ca186c2eabae66bb28fe4f57f839f9523f3e711
jnapolitan <julian.napolitan@gmail.com>
2018-11-30 13:32:32 -0800

    Setup widgets project

---
+++ widgets/widgets.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <h1>Nobody messes with Jeric</h1>,
+    document.getElementById('root')
+  );
+});
+
